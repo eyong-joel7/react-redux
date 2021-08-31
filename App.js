0a46ad534b3da456b430b3f654a4497012578b18
JOEL EYONG <62267332+eyong-joel7@users.noreply.github.com>
2021-08-31 16:34:14 +0300

    Replaced our custom Provider, context and connect functions with the ReactRedux library

--- App.js
+++ App.js
@@ -287,72 +287,26 @@ class App extends React.Component {
 }
 
 
-const ConnectedApp = connect((state) => ({
+const ConnectedApp = ReactRedux.connect((state) => ({
   loading: state.loading
 }))(App);
 
 
 
-const ConnectedGoal = connect((state) => ({
+const ConnectedGoal = ReactRedux.connect((state) => ({
   goals: state.goals
 }))(Goals);
 
 
-const ConnectedTodos = connect((state) => ({
+const ConnectedTodos = ReactRedux.connect((state) => ({
   todos: state.todos
 }))(Todos);
 
-const Context = React.createContext();
 
-function connect(mapStateToProps){
-  return (Component)=> {
-class Receiver extends React.Component{
-  componentDidMount(){
-    const {subscribe} = this.props.store
-    this.unsubscribe = subscribe(()=> {this.forceUpdate()})
-  }
-  componentWillUnMount(){
-this.unsubscribe();
-  }
-  render(){
-    const {dispatch, getState} = this.props.store;
-    const state = getState();
-    const stateNeeded = mapStateToProps(state)
-    return(
-    <Component {...stateNeeded} dispatch  = {dispatch}/>
-    )
-  }
-}
-    class ConnectedComponent extends React.Component{
-      render(){
-        return (
-          <Context.Consumer>
-            {
-              (store)=> <Receiver store = {store}/>
-            }
-          </Context.Consumer>
-        )
-      }
-    }
-    return ConnectedComponent
-
-  }
-}
-
-class Provider extends React.Component{
-
-  render(){
-    return (
-      <Context.Provider value = {this.props.store}>
-        {this.props.children}
-      </Context.Provider>
-    )
-  }
-}
 
 ReactDOM.render(
-  <Provider store={store}>
+  <ReactRedux.Provider store={store}>
     <ConnectedApp />
-  </Provider>,
+  </ReactRedux.Provider>,
   document.getElementById("app")
 );
